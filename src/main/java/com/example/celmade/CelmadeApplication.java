7f89306422cf5deec3d3383825320be4cc28b231
Yunsaehim <saehim26@naver.com>
2025-07-07 12:30:20 +0900

    create : 프로잭트 생성
    create : 프로잭트 생성

---
+++ src/main/java/com/example/celmade/CelmadeApplication.java
@@ -0,0 +1,13 @@
+package com.example.celmade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelmadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelmadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/celmade/CelmadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.celmade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelmadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
